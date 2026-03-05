9d01a8f59f6d0f2fd65138e246d0f7056fe60ed4
Md Aman <aman003245@gmail.com>
2026-03-05 12:08:59 +0530

    do

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ require('dotenv').config();
 const app = require('./src/app');
 const connectToDB = require('./src/config/database');
 
+// Database connection
 connectToDB();
+
+const PORT = process.env.PORT || 8080;
 
 
 app.listen(8080, () => {
-    console.log(`Server is running on 8080`);
-})
+    console.log(`🚀 Server running on port ${PORT}`);
+});
--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
-
+const authRouter = require('./routes/auth.routes')
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 
+
+
+// routes...
+app.use("/api/auth", authRouter);
 
 
 
