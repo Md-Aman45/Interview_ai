b81d23b069abe932d8c979637dde81f7b045cb01
Md Aman <aman003245@gmail.com>
2026-03-26 20:48:57 +0530

    mkajo

--- Frontend/src/features/auth/hook/userAuth.js
+++ Frontend/src/features/auth/hook/userAuth.js
@@ -12,8 +12,33 @@ export const useAuth = () => {
 
     const handleLogin = async ({ email, password }) => {
         setLoading(true)
+        const data = await login({ email, password })
+        setUser(data.user)
+        setLoading(false)
     }
 
+
+    const handleRegister = async ({ username, email, password }) => {
+        setLoading(true)
+        const data = await register({ username, email, password })
+        setUser(data.user)
+        setLoading(false)
+    }
+
+
+
+    const handleLogout = async () => {
+        setLoading(true)
+        const data = await logout()
+        setUser(null)
+        setLoading(false)
+    }
+
+
+
+    return { user, loading, handleRegister, handleLogin, handleLogout }
+
+    
 }
 
 
@@ -21,8 +46,3 @@ export const useAuth = () => {
 
 
 
-
-
-
-
-// github color code is change now but beleive today i do my work...
