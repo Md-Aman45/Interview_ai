43910b3269875f10bd2a371500ba526d284b6849
Md Aman <aman003245@gmail.com>
2026-03-31 20:22:23 +0530

    genAI

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,25 @@
 require('dotenv').config();
 const app = require('./src/app');
 const connectToDB = require('./src/config/database');
+const invokeGeminiAI = require('./src/services/ai.service');
 
-// Database connection
+
+// Database connection...
 connectToDB();
 
+
+
+// Test Gemini AI...
+invokeGeminiAI();
+
+
+
+// Start the server...
 const PORT = process.env.PORT || 8080;
 
 
 app.listen(8080, () => {
     console.log(`🚀 Server running on port ${PORT}`);
-
-
     console.log("🩺 Health Check");
     console.log("Server Status: OK");
 });
